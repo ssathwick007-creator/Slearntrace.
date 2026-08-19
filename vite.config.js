5b35d56bb2096d16889217c7c49d947ba807143a
ssathwick007-creator <ssathwick007@gmail.com>
2026-08-19 23:17:10 +0530

    i have updated  login page

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: './index.html',
+        login: './login.html',
+        profile: './profile.html'
       }
     }
   }
